4a6c3db874d0cba921d179aeaf4436a34d00e56b
i-prince <molatonny@gmail.com>
2023-12-19 18:33:51 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -2,37 +2,70 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
-from models import db, Bakery, BakedGood
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+# Define the base model with the SerializerMixin
+Base = declarative_base(cls=(db.Model, SerializerMixin))
+
+class Bakery(Base):
+    __tablename__ = 'bakeries'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    baked_goods = relationship('BakedGood', back_populates='bakery')
+
+class BakedGood(Base):
+    __tablename__ = 'baked_goods'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    bakery = relationship('Bakery', back_populates='baked_goods')
+
+# Apply the migration
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    return jsonify([b.serialize for b in bakeries])
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    if bakery:
+        return jsonify(bakery.serialize_with_baked_goods)
+    else:
+        return jsonify({'error': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([bg.serialize for bg in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+def get_most_expensive_baked_good():
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        return jsonify(most_expensive.serialize)
+    else:
+        return jsonify({'error': 'No baked goods found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
